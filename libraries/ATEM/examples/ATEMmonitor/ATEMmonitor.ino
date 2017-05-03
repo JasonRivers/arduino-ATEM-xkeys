8d55f2511ff7b1bf18c14a956ca73b44c37c6017
Jason Rivers <jason@jasonrivers.co.uk>
2017-05-03 10:46:40 +0100

    Add ATEM library from Skaarhoj

---
+++ libraries/ATEM/examples/ATEMmonitor/ATEMmonitor.ino
@@ -0,0 +1,78 @@
+/*****************
+ * Example: ATEM Monitor
+ * Connects to the Atem Switcher and outputs changes to Preview and Program on the Serial monitor (at 9600 baud)
+ *
+ * - kasper
+ */
+/*****************
+ * TO MAKE THIS EXAMPLE WORK:
+ * - You must have an Arduino with Ethernet Shield (or compatible such as "Arduino Ethernet", http://arduino.cc/en/Main/ArduinoBoardEthernet)
+ * - You must have an Atem Switcher connected to the same network as the Arduino - and you should have it working with the desktop software
+ * - You must make specific set ups in the below lines where the comment "// SETUP" is found!
+ */
+
+
+
+// Including libraries: 
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+
+//#include <MemoryFree.h>
+
+
+// MAC address and IP address for this *particular* Arduino / Ethernet Shield!
+// The MAC address is printed on a label on the shield or on the back of your device
+// The IP address should be an available address you choose on your subnet where the switcher is also present
+byte mac[] = { 
+  0x90, 0xA2, 0xDA, 0x0D, 0x6B, 0xB9 };      // <= SETUP!  MAC address of the Arduino
+IPAddress ip(192, 168, 10, 99);              // <= SETUP!  IP address of the Arduino
+
+
+// Include ATEM library and make an instance:
+// Connect to an ATEM switcher on this address and using this local port:
+// The port number is chosen randomly among high numbers.
+#include <ATEM.h>
+ATEM AtemSwitcher(IPAddress(192, 168, 10, 240), 56417);  // <= SETUP (the IP address of the ATEM switcher)
+
+
+
+// No-cost stream operator as described at 
+// http://arduiniana.org/libraries/streaming/
+template<class T>
+inline Print &operator <<(Print &obj, T arg)
+{  
+  obj.print(arg); 
+  return obj; 
+}
+
+
+
+void setup() { 
+
+    randomSeed(analogRead(5));
+  
+  // Start the Ethernet, Serial (debugging) and UDP:
+  Ethernet.begin(mac,ip);
+  Serial.begin(115200);
+  Serial << F("\n- - - - - - - -\nSerial Started\n");  
+
+  // Initialize a connection to the switcher:
+  AtemSwitcher.serialOutput(0x80);  // Remove or comment out this line for production code. Serial output may decrease performance!
+  AtemSwitcher.connect();
+
+  // Shows free memory:  
+//  Serial << F("freeMemory()=") << freeMemory() << "\n";
+}
+
+void loop() {
+  // Check for packets, respond to them etc. Keeping the connection alive!
+  // VERY important that this function is called all the time - otherwise connection might be lost because packets from the switcher is
+  // overlooked and not responded to.
+    AtemSwitcher.runLoop();
+
+  // If connection is gone anyway, try to reconnect:
+  if (AtemSwitcher.isConnectionTimedOut())  {
+    Serial << F("Connection to ATEM Switcher has timed out - reconnecting!\n");
+    AtemSwitcher.connect();
+  }  
+}
